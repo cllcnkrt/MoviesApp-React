a64772114e980ee441848b9cd34bf97cb6a807a0
cllcnkrt <cllcnkrt@outlook.com>
2021-06-05 18:51:39 +0300

    Movie list cards created.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 
 class App extends React.Component {
@@ -11,6 +10,13 @@ class App extends React.Component {
         "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
         "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg",
         "id": 7
+      },
+      {
+        "name": "Hostage",
+        "rating": "6.3",
+        "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/4hne3v6jN4MlCnhSkxOW7YspJhr.jpg",
+        "overview": "When a mafia accountant is taken hostage on his beat, a police officer – wracked by guilt from a prior stint as a negotiator – must negotiate the standoff, even as his own family is held captive by the mob.",
+        "id": 13
       },
       {
         "name": "The Matrix Reloaded",
@@ -28,18 +34,12 @@ class App extends React.Component {
       },
       {
         "name": "Blitz 007",
-        "rating": "11",
-        "overview": "A tough, renegade cop with a gay sidekick is dispatched to take down a serial killer who has been targeting police officers. AÇIKLAMA AÇIKLAMA",
+        "rating": "9.2",
+        "overview": "A tough, renegade cop with a gay sidekick is dispatched to take down a serial killer who has been targeting police officers.",
         "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qCPMjT8Ld8tvs1zs7LY2jpKlRIK.jpg",
         "id": 12
-      },
-      {
-        "name": "Hostage",
-        "rating": "6.3",
-        "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/4hne3v6jN4MlCnhSkxOW7YspJhr.jpg",
-        "overview": "When a mafia accountant is taken hostage on his beat, a police officer – wracked by guilt from a prior stint as a negotiator – must negotiate the standoff, even as his own family is held captive by the mob.",
-        "id": 13
       }
+      
     ]
   };
 
@@ -49,11 +49,11 @@ class App extends React.Component {
 
         <div className="row">
           <div className="col-lg-12">
-            <SearchBar />
+          
           </div>
         </div>
 
-        <MovieList />
+        <MovieList  movies ={this.state.movies}/>
 
       </div>
     );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,40 @@
 import React from "react";
 
-class MovieList extends React.Component {
-  render() {
+const MovieList = (props) => {
+  
     return (
       <div className="row">
-        <div className="col-lg-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg"
-              className="card-img-top"
-              alt="sample card image"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Sample Movie</h5>
-              <p className="card-text">Sample Movie Description</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <button type="button" className="btn btn-md btn-outline-danger">
-                  Delete
-                </button>
-                <h2>
-                  <span className="badge bg-primary" color= "red">9.0</span>
-                </h2>
+        {props.movies.map((movie) => (
+          <div className="col-lg-4" key= {movie.id}>
+            <div className="card mb-4 shadow-sm">
+              <img
+                src={movie.imageURL}
+                className="card-img-top"
+                alt="sample card image"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{movie.name}</h5>
+                <p className="card-text">{movie.overview}</p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <button
+                    type="button"
+                    className="btn btn-md btn-outline-danger"
+                  >
+                    Delete
+                  </button>
+                  <h2>
+                    <span className="badge bg-primary" color="red">
+                      {movie.rating}
+                    </span>
+                  </h2>
+                </div>
               </div>
-             
             </div>
           </div>
-        </div>
+        ))}
       </div>
     );
   }
-}
+
 
 export default MovieList;
